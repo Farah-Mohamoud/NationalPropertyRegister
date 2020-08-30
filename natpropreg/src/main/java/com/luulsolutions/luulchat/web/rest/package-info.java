a1bc405d0e970dbb47c4a66b47ee425825dcbd21
Farah-Mohamoud <Farah-Mohamoud.com>
2020-08-30 09:38:50 +0300

    initial backend and front end applications

---
+++ natpropreg/src/main/java/com/luulsolutions/luulchat/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.luulsolutions.luulchat.web.rest;
---
+++ natpropreg/src/main/java/com/luulsolutions/luulchat/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.luulsolutions.luulchat.web.rest.vm;
